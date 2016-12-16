ddba642b6fd93d245118a7f2bcbcf46d867884bd
Kudashev Serge <kudashevs@gmail.com>
2016-12-17 00:54:03 +0200

    Add LastModified plugin file with debug

--- LastModified.php
+++ LastModified.php
@@ -3,9 +3,11 @@ if ($modx->event->name == 'OnWebPagePrerender') {
 	$dtm = ($modx->resource->get('editedon'))?strtotime($modx->resource->get('editedon')):strtotime($modx->resource->get('createdon'));
 	if(empty($dtm)) { return ''; }
 
+	$modx->log(modX::LOG_LEVEL_INFO,$_SERVER['REQUEST_URI'].' use '.$dtm.' date');
 	if(!empty($_SERVER['HTTP_IF_MODIFIED_SINCE'])){
 		$ltm = strtotime($_SERVER['HTTP_IF_MODIFIED_SINCE']);
 		if($dtm <= $ltm){
+			$modx->log(modX::LOG_LEVEL_INFO,'Header 304 with document date '.$dtm.' and '.$ltm.' request date');
 			header('HTTP/1.0 304 Not Modified');
 			header('Last-Modified: '.gmdate('D, d M Y H:i:s',$dtm).' GMT');
 			header('Cache-control: private, max-age=3600');
